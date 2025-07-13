1b2296015ac46041bb3ff7c3ee7edb43b258d154
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2025-07-13 19:46:33 +0200

    Update to Unity v6.49 - UI

--- Assets/Scripts/UI/PanelDefaultButtonHighlighter.cs
+++ Assets/Scripts/UI/PanelDefaultButtonHighlighter.cs
@@ -1,17 +1,45 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
 
 public class PanelDefaultButtonHighlighter : MonoBehaviour
 {
     [SerializeField] private Button defaultHighlightedButton;
 
-    private void Start()
+    private Coroutine checkCoroutine;
+
+    private void OnEnable()
     {
         HighlightDefaultButton();
+        if (checkCoroutine != null) StopCoroutine(checkCoroutine);
+        checkCoroutine = StartCoroutine(ReselectIfNoneSelected());
+    }
+
+    private void OnDisable()
+    {
+        if (checkCoroutine != null)
+            StopCoroutine(checkCoroutine);
     }
 
     public void HighlightDefaultButton()
     {
-        if (defaultHighlightedButton) defaultHighlightedButton.Select();
+        if (defaultHighlightedButton == null) return;
+        EventSystem.current.SetSelectedGameObject(null);
+        if (!defaultHighlightedButton.gameObject.activeInHierarchy || !defaultHighlightedButton.interactable) return;
+        EventSystem.current.SetSelectedGameObject(defaultHighlightedButton.gameObject);
+    }
+
+    private IEnumerator ReselectIfNoneSelected()
+    {
+        while (true)
+        {
+            yield return new WaitForSecondsRealtime(3f);
+
+            if (EventSystem.current.currentSelectedGameObject == null)
+            {
+                HighlightDefaultButton();
+            }
+        }
     }
 }
